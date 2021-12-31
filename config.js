d5fbcab5c11cc0a21ad545de8c4e826ed193cd3f
hoangkhiem1804 <96908644+hoangkhiem1804@users.noreply.github.com>
2021-12-31 17:12:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!!!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introDesc: `Chúc mừng kỉ niệm 2 năm nhó 👩‍❤️‍💋‍👨
+    Cảm ơn em thời gian vừa qua
+    Đã ở bên anhhhhh
+    Đằng sau chữ yêu đây là thương ❣️❣️❣️`,
+    btnIntro: 'Ấn đi xem có gì 😜',
     title: 'Chúc mừng kỉ niệm 2 năm của chúng taaaa 😙',
     desc: 'Hy vọng 2024 chúng ta sẽ về chung 1 nhà ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h địa chỉ cũ nhé babe.',
     btnAccept: 'Okiiiii lun <3',
