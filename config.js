596201e1eb339d3116aa9c409be3317e69ffa617
hoangkhiem1804 <96908644+hoangkhiem1804@users.noreply.github.com>
2021-12-31 16:54:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Chúc mừng kỉ niệm 2 năm của chúng taaaa 😙',
+    desc: 'Hy vọng 2024 chúng ta sẽ về chung 1 nhà ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
